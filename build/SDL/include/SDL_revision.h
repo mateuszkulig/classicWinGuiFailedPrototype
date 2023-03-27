08ca4b82e7714367baae50d34b0a0d7f7c71c2a4
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-03-27 09:28:26 +0200

    initial file tree; cmake config

---
+++ build/SDL/include/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.26.0-324-gcd64e0b6e (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.26.0-324-gcd64e0b6e"
+#endif
